7f69351571805532c51f689d72ac4c72b620cfcf
Natela <akselerat96@gmail.com>
2025-05-25 10:15:33 +0300

    25.05.2025 first commit

---
+++ intensive/part_1/Bucket.java
@@ -0,0 +1,160 @@
+package aston.intensive.part_1;
+
+class Bucket<K, V>
+{
+    private Node<K, V> head;
+    private int size;
+
+    Bucket()
+    {
+        this.size = 0;
+    }
+
+    protected V put(int hash, K key, V value)
+    {
+        Node<K, V> insideNode = getMatchNode(hash, key);
+        V oldValue = null;
+        if (insideNode == null)
+        {
+            Node<K, V> outsideNode = new Node<>(hash, key, value, null);
+            link(outsideNode, head);
+            size++;
+        } else
+        {
+            oldValue = insideNode.getValue();
+            insideNode.setValue(value);
+        }
+        return oldValue;
+    }
+
+
+    protected V get(int hash, Object key)
+    {
+        Node<K, V> node = getMatchNode(hash, key);
+        return node != null ? node.getValue() : null;
+    }
+
+    protected V remove(int hash, Object key)
+    {
+        Node<K, V> prevNode = getPrevNode(hash, key);
+        if (prevNode != null)
+        {
+            Node<K, V> currentNode = prevNode.getNext();
+            makeRemove(currentNode, prevNode);
+            return currentNode.getValue();
+        }
+        return null;
+    }
+
+    protected boolean remove(int hash, Object key, Object value)
+    {
+        Node<K, V> prevNode = getPrevNode(hash, key);
+        if (prevNode != null)
+        {
+            Node<K, V> currentNode = prevNode.getNext();
+            if (currentNode.getValue().equals(value))
+            {
+
+                makeRemove(currentNode, prevNode);
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    protected int size()
+    {
+        return this.size;
+    }
+
+    protected Node<K, V> getHead()
+    {
+        return this.head;
+    }
+
+    private Node<K, V> getMatchNode(int hashOutside, Object keyOutside)
+    {
+        Node<K, V> current = head;
+        while (current != null)
+        {
+            if (hashOutside == current.getHash())
+            {
+                K keyCurrent = current.getKey();
+                if (sameKey(keyCurrent, keyOutside))
+                {
+                    return current;
+                }
+            }
+            current = current.getNext();
+        }
+        return null;
+    }
+
+    private Node<K, V> getPrevNode(int hashOutside, Object keyOutside)
+    {
+        Node<K, V> current = head;
+        Node<K, V> prev = null;
+        while (current != null)
+        {
+            if (hashOutside == current.getHash())
+            {
+                K keyCurrent = current.getKey();
+                if (sameKey(keyCurrent, keyOutside))
+                {
+                    if (current == head)
+                    {
+                        prev = new Node<>(0, null, null, head);
+                    }
+                    return prev;
+                }
+            }
+            prev = current;
+            current = current.getNext();
+        }
+        return prev;
+    }
+
+    private boolean sameKey(Object keyCurrent, Object keyOutside)
+    {
+        if (keyCurrent == keyOutside) return true;
+        return keyCurrent != null && keyCurrent.equals(keyOutside);
+    }
+
+    private void link(Node<K, V> prev, Node<K, V> current)
+    {
+        if (prev != null)
+        {
+            prev.setNext(current);
+            if (current == head)
+            {
+                setHead(prev);
+            }
+        }
+    }
+
+    private void makeRemove(Node<K, V> currentNode, Node<K, V> prevNode)
+    {
+        if (currentNode != head)
+        {
+            link(prevNode, currentNode.getNext());
+        }
+        unlink(currentNode);
+        size--;
+    }
+
+    private void unlink(Node<K, V> current)
+    {
+        if (current == head)
+        {
+            setHead(current.getNext());
+        }
+        current.setNext(null);
+    }
+
+    private void setHead(Node<K, V> node)
+    {
+        this.head = node;
+    }
+
+}
---
+++ intensive/part_1/MyHashMap.java
@@ -0,0 +1,147 @@
+package aston.intensive.part_1;
+
+public class MyHashMap<K, V>
+{
+    private static final int LIMIT_SIZE_BUCKET = 8;
+    static final int MAXIMUM_CAPACITY = 1 << 30;
+    private Bucket<K, V>[] buckets;
+    private int initialCapacity;
+
+    private float loadFactor;
+    private int threshold;
+    public MyHashMap()
+    {
+        this(16, 0.75f);
+    }
+
+    public MyHashMap(int initialCapacity)
+    {
+        this(initialCapacity, 0.75f);
+    }
+
+    public MyHashMap(int initialCapacity, float loadFactor)
+    {
+        initialCapacity = getCorrected(initialCapacity);
+        check(loadFactor);
+
+        this.initialCapacity = bucketsSizeFor(initialCapacity);
+        this.loadFactor = loadFactor;
+
+        this.threshold = createThreshold();
+        this.buckets = createBuckets(this.initialCapacity);
+    }
+    public V put(K key, V value)
+    {
+        int hashCode = key.hashCode();
+        int index = getIndexBucket(hashCode);
+        V val = buckets[index].put(hashCode, key, value);;
+        if (size() > threshold || buckets[index].size() > LIMIT_SIZE_BUCKET)
+        {
+            rehashing();
+        }
+        return val;
+    }
+
+    public V get(Object key)
+    {
+        int hashCode = key.hashCode();
+        int index = getIndexBucket(hashCode);
+        return buckets[index].get(hashCode, key);
+    }
+
+    public V remove(Object key)
+    {
+        int hashCode = key.hashCode();
+        int index = getIndexBucket(hashCode);
+        return buckets[index].remove(hashCode, key);
+    }
+
+    public boolean remove(Object key, Object value)
+    {
+        int hashCode = key.hashCode();
+        int index = getIndexBucket(hashCode);
+        return buckets[index].remove(hashCode, key, value);
+    }
+
+    public int size()
+    {
+        int sum = 0;
+        for (int i = 0; i < buckets.length; i++)
+        {
+            sum += buckets[i].size();
+        }
+        return sum;
+    }
+
+    private static  int getCorrected(int initialCapacity)
+    {
+        if (initialCapacity < 0)
+        {
+            throw new IllegalArgumentException("Illegal initial capacity: " +
+                    initialCapacity);
+        }
+        if (initialCapacity > MAXIMUM_CAPACITY)
+            return MAXIMUM_CAPACITY;
+        return initialCapacity;
+    }
+    private static int bucketsSizeFor(int initialCapacity)
+    {
+        int correctCapacity = initialCapacity - 1;
+        correctCapacity = correctCapacity | (correctCapacity >>> 1);
+        correctCapacity = correctCapacity | (correctCapacity >>> 2);
+        correctCapacity = correctCapacity | (correctCapacity >>> 4);
+        correctCapacity = correctCapacity | (correctCapacity >>> 8);
+        correctCapacity = correctCapacity | (correctCapacity >>> 16);
+        if (correctCapacity < 0)
+        {
+            return 1;
+        } else if (correctCapacity >= MAXIMUM_CAPACITY)
+        {
+            return MAXIMUM_CAPACITY;
+        }
+        return correctCapacity + 1;
+    }
+    private void check(float loadFactor)
+    {
+        if (loadFactor <= 0 || Float.isNaN(loadFactor))
+            throw new IllegalArgumentException("Illegal load factor: " +
+                    loadFactor);
+    }
+    private int createThreshold()
+    {
+        return Math.round(this.initialCapacity * this.loadFactor);
+    }
+    private Bucket<K,V> [] createBuckets(int initialCapacity)
+    {
+        Bucket<K,V> [] buckets = new Bucket[initialCapacity];
+        for (int i = 0; i < initialCapacity; i++)
+        {
+            buckets[i] = new Bucket<K, V>();
+        }
+        return buckets;
+    }
+
+    private int getIndexBucket(int hash)
+    {
+        return hash & (buckets.length - 1);
+    }
+
+    private void rehashing()
+    {
+        int newInitialCapacity = getCorrected(initialCapacity * 2);
+        Bucket<K,V> [] oldBuckets = this.buckets;
+        this.buckets = createBuckets(newInitialCapacity);
+        this.initialCapacity = newInitialCapacity;
+        this.threshold = createThreshold();
+
+        for (int i = 0; i < oldBuckets.length; i++)
+        {
+            Node<K,V> current = oldBuckets[i].getHead();
+            while (current != null)
+            {
+                put(current.getKey(), current.getValue());
+                current = current.getNext();
+            }
+        }
+    }
+}
